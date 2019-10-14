4fe050f31752b8718d79a7d003becf75403b8b99
Jason Taylor <jasongt@outlook.com>
2019-10-14 14:16:14 +1100

    Add support for AutoMapper.

---
+++ src/Application/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace CleanArchitecture.Application.Common.Mappings
+{
+    public interface IMapFrom<T>
+    {   
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
---
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace CleanArchitecture.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i => 
+                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
