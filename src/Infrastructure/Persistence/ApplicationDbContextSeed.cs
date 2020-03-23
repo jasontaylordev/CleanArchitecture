fe4caf6286cc4f5a6c71621733c01315a6aebc6f
Jason Taylor <jasongt@outlook.com>
2020-03-23 22:39:41 +1000

    Update default user

--- src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -9,11 +9,11 @@ namespace CleanArchitecture.Infrastructure.Persistence
     {
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
         {
-            var defaultUser = new ApplicationUser { UserName = "jason@clean-architecture", Email = "jason@clean-architecture" };
+            var defaultUser = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
 
             if (userManager.Users.All(u => u.Id != defaultUser.Id))
             {
-                await userManager.CreateAsync(defaultUser, "CleanArchitecture!");
+                await userManager.CreateAsync(defaultUser, "Administrator1!");
             }
         }
     }
