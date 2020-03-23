f5be91ba2d79ffdda7d17185f8cdaca5e913d808
Jason Taylor <jasongt@outlook.com>
2020-03-23 23:52:48 +1000

    Only validate if there are validatiors associated with the request.

--- src/Application/Common/Behaviours/RequestValidationBehavior.cs
+++ src/Application/Common/Behaviours/RequestValidationBehavior.cs
@@ -20,17 +20,20 @@ namespace CleanArchitecture.Application.Common.Behaviours
 
         public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            var context = new ValidationContext(request);
-
-            var failures = _validators
-                .Select(v => v.Validate(context))
-                .SelectMany(result => result.Errors)
-                .Where(f => f != null)
-                .ToList();
-
-            if (failures.Count != 0)
+            if (_validators.Any())
             {
-                throw new ValidationException(failures);
+                var context = new ValidationContext(request);
+
+                var failures = _validators
+                    .Select(v => v.Validate(context))
+                    .SelectMany(result => result.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
             }
 
             return next();
