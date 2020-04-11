6361a1aebe42127ac0855c3df8f627d9404d3f03
Jason Taylor <jasongt@outlook.com>
2020-04-11 15:08:20 +1000

    Clean-up request logger tests.

---
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Application.Common.Behaviours;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.TodoItems.Commands.CreateTodoItem;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.UnitTests.Common.Behaviours
+{
+    public class RequestLoggerTests
+    {
+        private readonly Mock<ILogger<CreateTodoItemCommand>> _logger;
+        private readonly Mock<ICurrentUserService> _currentUserService;
+        private readonly Mock<IIdentityService> _identityService;
+
+
+        public RequestLoggerTests()
+        {
+            _logger = new Mock<ILogger<CreateTodoItemCommand>>();
+
+            _currentUserService = new Mock<ICurrentUserService>();
+
+            _identityService = new Mock<IIdentityService>();
+        }
+
+        [Test]
+        public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+        {
+            _currentUserService.Setup(x => x.UserId).Returns("Administrator");
+
+            var requestLogger = new RequestLogger<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+            await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+
+            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+        {
+            var requestLogger = new RequestLogger<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+            await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+
+            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
+        }
+    }
+}
