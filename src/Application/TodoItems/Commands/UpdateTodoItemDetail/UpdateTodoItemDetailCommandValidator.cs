ed57d3a7fa4e332a69570ce2984342d8036b1431
Jason Taylor <jasongt@outlook.com>
2020-08-04 21:59:07 +1000

    Remove validator.

--- src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommandValidator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CleanArchitecture.Application.TodoItems.Commands.UpdateTodoItemDetail
-{
-    public class UpdateTodoItemDetailCommandValidator
-    {
-        // TODO: Complete Validator
-    }
-}
