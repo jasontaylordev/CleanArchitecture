14d2164293c7a58e3385ba41a49cadf36ddc04be
Abdulateef <afees@hollaport.net>
2020-08-27 12:24:34 +0100

    added generic exeption to some non validation errors

---
+++ src/Application/Common/Exceptions/GenericException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CleanArchitecture.Application.Common.Exceptions
+{
+    public class GenericException
+    {
+        public GenericException()
+        {
+        }
+    }
+}
