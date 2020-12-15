ae447ba8b04ce8b7a516f0870498434a9853308e
SB2\jordan <jordan@krisis.ca>
2020-12-15 14:36:08 -0700

    upgrade angular to 11 accoring to upgrade guide: https://update.angular.io/?l=2&v=10.0-11.0

--- src/WebUI/ClientApp/src/api-authorization/login/login.component.spec.ts
+++ src/WebUI/ClientApp/src/api-authorization/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -6,7 +6,7 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
     })
--- src/WebUI/ClientApp/src/api-authorization/logout/logout.component.spec.ts
+++ src/WebUI/ClientApp/src/api-authorization/logout/logout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
 
@@ -6,7 +6,7 @@ describe('LogoutComponent', () => {
   let component: LogoutComponent;
   let fixture: ComponentFixture<LogoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LogoutComponent ]
     })
