4e205dc1f472253452ba9b53ca24fb078698812e
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:30:05 +0300

    ⛏ Configure EasyCache in Service Collection extension.

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,11 +1,17 @@
 ﻿using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure.Caching;
+using CleanArchitecture.Infrastructure.Caching.Utilities;
 using CleanArchitecture.Infrastructure.Files;
 using CleanArchitecture.Infrastructure.Identity;
 using CleanArchitecture.Infrastructure.Persistence;
 using CleanArchitecture.Infrastructure.Services;
+using EasyCache.MemCache.Extensions;
+using EasyCache.Memory.Extensions;
+using EasyCache.Redis.Extensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -51,6 +57,32 @@ namespace CleanArchitecture.Infrastructure
             {
                 options.AddPolicy("CanPurge", policy => policy.RequireRole("Administrator"));
             });
+
+            if (configuration.GetValue<bool>(Constants.CACHE_OPTIONS_IS_ACTIVE))
+            {
+                switch (configuration.GetValue<string>(Constants.CACHE_OPTIONS_PROVIDER_NAME))
+                {
+                    case Constants.CACHE_REDIS:
+                        RedisCacheOption redisCacheOption = new RedisCacheOption();
+                        configuration.GetSection(Constants.CACHE_OPTIONS_REDIS).Bind(redisCacheOption, c => c.BindNonPublicProperties = true);
+                        services.AddEasyRedisCache(options =>
+                        {
+                            options.Configuration = redisCacheOption.Configuration;
+                            options.InstanceName = redisCacheOption.InstanceName;
+                        });
+                        break;
+                    case Constants.CACHE_MEMORY:
+                        services.AddEasyMemoryCache();
+                        break;
+                    case Constants.CACHE_MEMCACHE:
+                        MemCacheOption memCacheOption = new MemCacheOption();
+                        configuration.GetSection(Constants.CACHE_OPTIONS_MEMCACHE).Bind(memCacheOption, c => c.BindNonPublicProperties = true);
+                        services.AddEasyMemCache(options => options.AddServer(memCacheOption.ServerName, memCacheOption.Port));
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             return services;
         }
