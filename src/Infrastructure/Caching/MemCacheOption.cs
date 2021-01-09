5d591c31c7c68b997c8c0a7c8f709a586c968fbf
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:27:28 +0300

    ⛏ Add Option class for MemCache

---
+++ src/Infrastructure/Caching/MemCacheOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Caching
+{
+    /// <summary>
+    /// This class includes cache options for MemCache
+    /// </summary>
+    internal class MemCacheOption
+    {
+        /// <summary>
+        /// Server Name For Example : localhost
+        /// </summary>
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// Port number of Server. For Example = 11211
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
