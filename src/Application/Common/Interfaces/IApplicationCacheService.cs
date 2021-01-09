8c2f0332dd2b4a6994ce08039c269ab00ad9406b
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 21:54:04 +0300

    🔨 Add interface for Application Cache.

---
+++ src/Application/Common/Interfaces/IApplicationCacheService.cs
@@ -0,0 +1,17 @@
+﻿using EasyCache.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Interfaces
+{
+    /// <summary>
+    /// This interface base methods for Cache
+    /// </summary>
+    public interface IApplicationCacheService : IEasyCacheService
+    {
+
+    }
+}
