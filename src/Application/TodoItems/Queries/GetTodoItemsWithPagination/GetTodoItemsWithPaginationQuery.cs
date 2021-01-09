9f78fd7d5505001e1b0e558c5aaebf9feb5e63e2
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:35:33 +0300

    ⛏ Add Sample code block for EasyCache.

--- src/Application/TodoItems/Queries/GetTodoItemsWithPagination/GetTodoItemsWithPaginationQuery.cs
+++ src/Application/TodoItems/Queries/GetTodoItemsWithPagination/GetTodoItemsWithPaginationQuery.cs
@@ -4,7 +4,10 @@ using CleanArchitecture.Application.Common.Interfaces;
 using CleanArchitecture.Application.Common.Mappings;
 using CleanArchitecture.Application.Common.Models;
 using CleanArchitecture.Application.TodoLists.Queries.GetTodos;
+using EasyCache.Core.Abstractions;
+using EasyCache.Core.Extensions;
 using MediatR;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,20 +25,27 @@ namespace CleanArchitecture.Application.TodoItems.Queries.GetTodoItemsWithPagina
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IEasyCacheService _easyCacheService;
 
-        public GetTodoItemsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+        public GetTodoItemsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper, IEasyCacheService easyCacheService)
         {
             _context = context;
             _mapper = mapper;
+            _easyCacheService = easyCacheService;
         }
 
         public async Task<PaginatedList<TodoItemDto>> Handle(GetTodoItemsWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            return await _easyCacheService.GetAndSetAsync(request.ListId.ToString(), async () => await GetTodoItemAsync(request), TimeSpan.FromMinutes(1));
+        }
+
+        private async Task<PaginatedList<TodoItemDto>> GetTodoItemAsync(GetTodoItemsWithPaginationQuery request)
         {
             return await _context.TodoItems
                 .Where(x => x.ListId == request.ListId)
                 .OrderBy(x => x.Title)
                 .ProjectTo<TodoItemDto>(_mapper.ConfigurationProvider)
-                .PaginatedListAsync(request.PageNumber, request.PageSize); ;
+                .PaginatedListAsync(request.PageNumber, request.PageSize);
         }
     }
 }
