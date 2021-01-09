3467091e3d791e40241c78cdebcf843990752d76
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:28:24 +0300

    ⛏ Add Option class for Redis Cache.

---
+++ src/Infrastructure/Caching/RedisCacheOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Caching
+{
+    /// <summary>
+    /// This class includes cache options for Redis Cache.
+    /// </summary>
+    internal class RedisCacheOption
+    {
+        /// <summary>
+        /// For Example : localhost
+        /// </summary>
+        public string Configuration { get; set; }
+
+        /// <summary>
+        /// Instance name For Example : CleanArchitecture
+        /// </summary>
+        public string InstanceName { get; set; }
+    }
+}
