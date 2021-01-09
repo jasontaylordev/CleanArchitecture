f2d7be27f68dba29770f3efe22c1fb11b74eeb98
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:30:30 +0300

    ⛏ Add Application Builder extension for MemCache.

---
+++ src/Infrastructure/Caching/Extensions/ApplicationBuilder.cs
@@ -0,0 +1,31 @@
+﻿using EasyCache.MemCache.Extensions;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Caching.Extensions
+{
+    /// <summary>
+    /// This class includes extension method for EasyMemCache.
+    /// </summary>
+    public static class ApplicationBuilder
+    {
+        /// <summary>
+        /// Use EasyMemCache
+        /// </summary>
+        /// <param name="app">
+        /// Microsoft.AspNetCore.Builder.IApplicationBuilder
+        /// </param>
+        /// <returns>
+        /// Microsoft.AspNetCore.Builder.IApplicationBuilder
+        /// </returns>
+        public static IApplicationBuilder UseEasyMemCache(this IApplicationBuilder app)
+        {
+            app.ApplyEasyMemCache();
+            return app;
+        }
+    }
+}
