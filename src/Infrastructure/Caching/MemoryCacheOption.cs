0f8c8ef72251f71b9007cc2c28a42cd93893c1d9
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 20:27:48 +0300

    ⛏ Add Option class for Memory Cache.

---
+++ src/Infrastructure/Caching/MemoryCacheOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Caching
+{
+    /// <summary>
+    /// This class includes cache options for MemoryCache.
+    /// </summary>
+    internal class MemoryCacheOption
+    {
+    }
+}
