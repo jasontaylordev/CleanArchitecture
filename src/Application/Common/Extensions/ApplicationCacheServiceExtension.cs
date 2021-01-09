ff9699b44650eeeca5e69d868146fa55c1112279
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-09 21:54:24 +0300

    🔨 Add extensions for Application cache service

---
+++ src/Application/Common/Extensions/ApplicationCacheServiceExtension.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Extensions
+{
+    public static class ApplicationCacheServiceExtension
+    {
+        public static T GetAndSet<T>(this IApplicationCacheService applicationCacheService, string key, Func<T> getResult, TimeSpan expireTime)
+        {
+            var data = applicationCacheService.Get<T>(key);
+
+            if (data == null)
+            {
+                data = getResult();
+                applicationCacheService.Set(key, data, expireTime);
+            }
+
+            return data;
+        }
+
+        public static T GetAndSetAsync<T>(this IApplicationCacheService applicationCacheService, string key, Func<T> getResult, TimeSpan expireTime)
+        {
+            var data = applicationCacheService.Get<T>(key);
+
+            if (data == null)
+            {
+                data = getResult();
+                applicationCacheService.Set(key, data, expireTime);
+            }
+
+            return data;
+        }
+    }
+}
