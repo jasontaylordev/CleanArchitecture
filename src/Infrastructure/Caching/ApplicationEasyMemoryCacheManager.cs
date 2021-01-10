445f43218a3465130622cd39569ad9640aa46077
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-10 14:25:22 +0300

    ♻ 🔥 Remove unnecessary override methods.

--- src/Infrastructure/Caching/ApplicationEasyMemoryCacheManager.cs
+++ src/Infrastructure/Caching/ApplicationEasyMemoryCacheManager.cs
@@ -14,35 +14,5 @@ namespace CleanArchitecture.Infrastructure.Caching
         public ApplicationEasyMemoryCacheManager(IMemoryCache memoryCache) : base(memoryCache)
         {
         }
-
-        public override T Get<T>(string key)
-        {
-            return base.Get<T>(key);
-        }
-
-        public override Task<T> GetAsync<T>(string key)
-        {
-            return base.GetAsync<T>(key);
-        }
-
-        public override void Remove<T>(string key)
-        {
-            base.Remove<T>(key);
-        }
-
-        public override Task RemoveAsync<T>(string key)
-        {
-            return base.RemoveAsync<T>(key);
-        }
-
-        public override void Set<T>(string key, T value, TimeSpan expireTime)
-        {
-            base.Set(key, value, expireTime);
-        }
-
-        public override Task SetAsync<T>(string key, T value, TimeSpan expireTime)
-        {
-            return base.SetAsync(key, value, expireTime);
-        }
     }
 }
