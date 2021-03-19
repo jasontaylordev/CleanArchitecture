21d442fc6930c0fa73dca8da5800d871258ce168
Martín Landart <martinjl95@hotmail.com>
2021-03-19 19:13:53 -0300

    Make ForbiddenAccessException conform to ISerializable

--- src/Application/Common/Exceptions/ForbiddenAccessException.cs
+++ src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace CleanArchitecture.Application.Common.Exceptions
 {
+    [Serializable]
     public class ForbiddenAccessException : Exception
     {
         public ForbiddenAccessException() : base() { }
+
+        protected ForbiddenAccessException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
---
+++ tests/Application.UnitTests/Common/Exceptions/ForbiddenAccessExceptionTests.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Application.Common.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CleanArchitecture.Application.UnitTests.Common.Exceptions
+{
+    public class ForbiddenAccessExceptionTests
+    {
+        [Test]
+        public void ShouldBeSerializable()
+        {
+            new ForbiddenAccessException().Should().BeBinarySerializable();
+        }
+    }
+}
