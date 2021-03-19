f40ed9d28fa0ac9ec2f6d9a17a1fab8d74d01403
Martín Landart <martinjl95@hotmail.com>
2021-03-19 19:14:01 -0300

    Make NotFoundException conform to ISerializable

--- src/Application/Common/Exceptions/NotFoundException.cs
+++ src/Application/Common/Exceptions/NotFoundException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace CleanArchitecture.Application.Common.Exceptions
 {
+    [Serializable]
     public class NotFoundException : Exception
     {
         public NotFoundException()
@@ -21,6 +23,11 @@ namespace CleanArchitecture.Application.Common.Exceptions
 
         public NotFoundException(string name, object key)
             : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
---
+++ tests/Application.UnitTests/Common/Exceptions/NotFoundExceptionTests.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Application.Common.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CleanArchitecture.Application.UnitTests.Common.Exceptions
+{
+    public class NotFoundExceptionTests
+    {
+        [Test]
+        public void ShouldBeBinarySerializable()
+        {
+            new NotFoundException("EntityName", 1).Should().BeBinarySerializable();
+        }
+    }
+}
