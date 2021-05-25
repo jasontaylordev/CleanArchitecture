5e96196186a8a87f05ee8afc7abc14cc9fdfda24
Rodney Cabahug <rodney.cabahug@gmail.com>
2021-05-25 22:11:45 +1200

    Upgrade Angular to 11.2.14

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -126,7 +126,7 @@ namespace CleanArchitecture.WebUI
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseAngularCliServer(npmScript: "start");
                     spa.UseProxyToSpaDevelopmentServer(Configuration["SpaBaseUrl"] ?? "http://localhost:4200");
                 }
             });
