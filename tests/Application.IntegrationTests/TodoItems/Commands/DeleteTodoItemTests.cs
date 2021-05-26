5b6d8c7881cbc705bff6e0cb7df551ab73073ef2
pajawojciech <pajawojciech@gmail.com>
2021-05-26 13:31:16 +0200

    Change parameter variable
    FindAsync shoud look for TodoItem with itemId, not listId

--- tests/Application.IntegrationTests/TodoItems/Commands/DeleteTodoItemTests.cs
+++ tests/Application.IntegrationTests/TodoItems/Commands/DeleteTodoItemTests.cs
@@ -41,7 +41,7 @@ namespace CleanArchitecture.Application.IntegrationTests.TodoItems.Commands
                 Id = itemId
             });
 
-            var list = await FindAsync<TodoItem>(listId);
+            var list = await FindAsync<TodoItem>(itemId);
 
             list.Should().BeNull();
         }
