e26636d9424fc65c5da72e02c18bb03808783f16
Jason Taylor <jasongt@outlook.com>
2021-07-11 08:01:28 +1000

    Quick clean-up

--- src/WebUI/ClientApp/src/app/token/token.component.ts
+++ src/WebUI/ClientApp/src/app/token/token.component.ts
@@ -1,42 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthorizeService } from '../../api-authorization/authorize.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthorizeService } from "../../api-authorization/authorize.service";
+
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-token-component',
-  templateUrl: './token.component.html'
+  selector: "app-token-component",
+  templateUrl: "./token.component.html",
 })
 export class TokenComponent implements OnInit {
-
   token: string;
   isError: boolean;
   isCopied: boolean;
 
-  constructor(private authorizeService: AuthorizeService) { }
+  faCopy = faCopy;
+
+  constructor(private authorizeService: AuthorizeService) {}
 
   ngOnInit(): void {
     this.isCopied = false;
-    this.authorizeService.getAccessToken()
-      .subscribe(t => {
-        this.token = t;
+    this.authorizeService.getAccessToken().subscribe(
+      (t) => {
+        this.token = "Bearer " + t;
         this.isError = false;
-      }, err => {
-          this.isError = true;
-      });
+      },
+      (err) => {
+        this.isError = true;
+      }
+    );
   }
 
   copyToClipboard(): void {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
+    const selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
     selBox.value = this.token;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(selBox);
     this.isCopied = true;
   }
-
 }
