b25670604e27c3a28e2f551f7db0e896e8ace142
Moein Mohammadian <mrmmohammadian@gmail.com>
2021-08-01 17:09:11 +0430

    Changed 'pageIndex' variable name to 'pageNumber' in PaginatedList.cs

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -9,28 +9,28 @@ namespace CleanArchitecture.Application.Common.Models
     public class PaginatedList<T> 
     {
         public List<T> Items { get; }
-        public int PageIndex { get; }
+        public int PageNumber { get; }
         public int TotalPages { get; }
         public int TotalCount { get; }
 
-        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
         {
-            PageIndex = pageIndex;
+            PageNumber = pageNumber;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             TotalCount = count;
             Items = items;
         }
 
-        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasPreviousPage => PageNumber > 1;
 
-        public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasNextPage => PageNumber < TotalPages;
 
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
-            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
 
-            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
         }
     }
 }
