87de33cfa193fce6e64eeb1f18a7e9c1e3393e45
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-05 21:46:24 +0300

    The baseConfiguration class was created and implemented from here for the repetitive title field in the configuration classes and for the properties that may come later.

---
+++ src/Infrastructure/Persistence/Configurations/BaseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Configurations
+{
+    public class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : CommonEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+           // builder.Property<int>(x => x.Id).HasColumnName(@"Id").IsRequired(true).UseIdentityColumn().ValueGeneratedOnAdd();
+
+            builder.Property(t => t.Title).HasMaxLength(200).IsRequired();
+        }
+    }
+}
--- src/Infrastructure/Persistence/Configurations/TodoListConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/TodoListConfiguration.cs
@@ -1,19 +1,15 @@
 ﻿using CleanArchitecture.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CleanArchitecture.Infrastructure.Persistence.Configurations
 {
-    public class TodoListConfiguration : IEntityTypeConfiguration<TodoList>
+    public class TodoListConfiguration : BaseConfiguration<TodoList>
     {
-        public void Configure(EntityTypeBuilder<TodoList> builder)
+        public override void Configure(EntityTypeBuilder<TodoList> builder)
         {
-            builder.Property(t => t.Title)
-                .HasMaxLength(200)
-                .IsRequired();
+            builder.OwnsOne(b => b.Colour);
 
-            builder
-                .OwnsOne(b => b.Colour);
+            base.Configure(builder);
         }
     }
 }
