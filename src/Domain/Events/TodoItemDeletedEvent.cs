047b098040f837b6ca3c31dd83d1d3bb33bd4f8d
Hasan Arab Borzo <30981174+nbbdev@users.noreply.github.com>
2021-11-10 03:57:04 +0330

    Solve the problem of publishing deleted events (#454)

---
+++ src/Domain/Events/TodoItemDeletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Events
+{
+    public class TodoItemDeletedEvent : DomainEvent
+    {
+        public TodoItemDeletedEvent(TodoItem item)
+        {
+            Item = item;
+        }
+
+        public TodoItem Item { get; }
+    }
+}
