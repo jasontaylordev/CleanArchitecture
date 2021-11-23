f7dbdcb109b5f8d76e235a2315490d8d93f465f1
Matt Goldman <matt.goldman@outlook.com>
2021-11-23 15:58:41 +1100

    Added dev env conditions for token in angular app

--- src/WebUI/ClientApp/src/app/app-routing.module.ts
+++ src/WebUI/ClientApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
+import { DevEnvGuard } from './nav-menu/dev-env.guard';
 import { TodoComponent } from './todo/todo.component';
 import { TokenComponent } from './token/token.component';
 
@@ -13,7 +14,7 @@ export const routes: Routes = [
   { path: 'fetch-data', component: FetchDataComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'todo', component: TodoComponent, canActivate: [AuthorizeGuard] },
-  { path: 'token', component: TokenComponent, canActivate: [AuthorizeGuard] }
+  { path: 'token', component: TokenComponent, canActivate: [AuthorizeGuard, DevEnvGuard] }
 ];
 
 @NgModule({
