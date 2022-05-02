e5a3e58e5b9adf9720ce1119853beceeb3bdad4b
ramax495 <ramax@inbox.ru>
2022-05-02 21:32:02 +0300

    Adding .AsNoTracking() for list projections

--- src/Application/Common/Mappings/MappingExtensions.cs
+++ src/Application/Common/Mappings/MappingExtensions.cs
@@ -7,9 +7,9 @@ namespace CleanArchitecture.Application.Common.Mappings;
 
 public static class MappingExtensions
 {
-    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize)
-        => PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize);
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+        => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
 
-    public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration)
-        => queryable.ProjectTo<TDestination>(configuration).ToListAsync();
+    public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
+        => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
 }
