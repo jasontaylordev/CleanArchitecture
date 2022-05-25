552e006c972bbee578eb482065ec23b1a4a75f09
Jason Taylor <jasontaylor@ssw.com.au>
2022-05-25 11:15:11 +1000

    ✅ Switch to SpecFlow with Playwright, add login failed test.

---
+++ tests/WebUI.AcceptanceTests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using CleanArchitecture.WebUI.AcceptanceTests.Pages;
+global using BoDi;
+global using FluentAssertions;
+global using Microsoft.Playwright;
+global using TechTalk.SpecFlow;
---
+++ tests/WebUI.AcceptanceTests/Pages/BasePage.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchitecture.WebUI.AcceptanceTests.Pages;
+
+public abstract class BasePage
+{
+    public abstract string PagePath { get; }
+
+    public abstract IBrowser Browser { get; }
+
+    public abstract IPage Page { get; }
+
+    public async Task GotoAsync()
+    {
+        await Page.GotoAsync(PagePath);
+    }
+}
