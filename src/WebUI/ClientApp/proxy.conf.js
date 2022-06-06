8278f225c4d4741c4dfc33f6fb2e3242be215b7c
Jason Taylor <jasontaylor@ssw.com.au>
2022-06-06 19:54:03 +1000

    🐛 Update proxy.conf.js to use correct port for back end. With the correct port, the front end can now be started separately from the back end.

--- src/WebUI/ClientApp/proxy.conf.js
+++ src/WebUI/ClientApp/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:61846';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
 
 const PROXY_CONFIG = [
   {
