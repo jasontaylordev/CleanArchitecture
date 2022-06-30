5718c3d94a4618461c8ef139f2a672f6b9ad41aa
deceptiveSimplicity <christian.wengel@gmail.com>
2022-06-30 22:37:56 +0200

    Fix events created in EventHandler aren't published

--- src/Infrastructure/Common/MediatorExtensions.cs
+++ src/Infrastructure/Common/MediatorExtensions.cs
@@ -5,21 +5,29 @@ namespace MediatR;
 
 public static class MediatorExtensions
 {
-    public static async Task DispatchDomainEvents(this IMediator mediator, DbContext context) 
+    public static async Task DispatchDomainEvents(this IMediator mediator, DbContext context)
     {
-        var entities = context.ChangeTracker
+        var entities = GetEntitiesWithPendingEvents(context);
+
+        while (entities.Any())
+        {
+            foreach (var entity in entities)
+            foreach (var domainEvent in entity.DomainEvents.ToList())
+            {
+                await mediator.Publish(domainEvent);
+                entity.RemoveDomainEvent(domainEvent);
+            }
+
+            entities = GetEntitiesWithPendingEvents(context);
+        }
+    }
+
+    private static List<BaseEntity> GetEntitiesWithPendingEvents(DbContext context)
+    {
+        return context.ChangeTracker
             .Entries<BaseEntity>()
             .Where(e => e.Entity.DomainEvents.Any())
             .Select(e => e.Entity)
             .ToList();
-
-        var domainEvents = entities
-            .SelectMany(e => e.DomainEvents)
-            .ToList();
-
-        entities.ForEach(e => e.ClearDomainEvents());
-
-        foreach (var domainEvent in domainEvents)
-            await mediator.Publish(domainEvent);
     }
 }
