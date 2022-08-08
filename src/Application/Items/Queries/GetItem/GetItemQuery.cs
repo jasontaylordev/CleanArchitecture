fe6adca2c34d583edf9a4724d16d31fa5c4f490a
Amado <amadoa@gmail.com>
2022-08-08 17:17:26 -0400

    Add serilog console logging

--- src/Application/Items/Queries/GetItem/GetItemQuery.cs
+++ src/Application/Items/Queries/GetItem/GetItemQuery.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using CleanArchitecture.Application.Common.Interfaces;
-using CleanArchitecture.Application.Common.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Application.Items.Queries.GetItem;
 
@@ -17,21 +17,25 @@ public class GetItemsQueryHandler : IRequestHandler<GetItemsQuery, ItemDto?>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
+    private readonly ILogger<GetItemsQueryHandler> _logger;
 
-    public GetItemsQueryHandler(IApplicationDbContext context, IMapper mapper)
+    public GetItemsQueryHandler(IApplicationDbContext context, IMapper mapper, ILogger<GetItemsQueryHandler> logger)
     {
         _context = context;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<ItemDto?> Handle(GetItemsQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Start item query");
         var result = await _context.ItemMaster
                 .AsNoTracking()
                 .Where(i => i.ItemNumber == request.ItemNumber)
                 .ProjectTo<ItemDto>(_mapper.ConfigurationProvider)
                 .SingleOrDefaultAsync(cancellationToken);
 
+        _logger.LogInformation("End item query");
         return result;
     }
 }
--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,60 +1,97 @@
 //copy db2 license
-var licensePath = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location)?.FullName + "/clidriver/license/db2consv_ee.lic";
-if (!File.Exists(licensePath))
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+
+try
 {
-    File.Copy("/db2/license/db2consv_ee.lic", licensePath);
+    Log.Information("Service Starting...");
+    var licensePath = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location)?.FullName + "/clidriver/license/db2consv_ee.lic";
+    if (!File.Exists(licensePath))
+    {
+        File.Copy("/db2/license/db2consv_ee.lic", licensePath);
+    }
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Setup logging
+    var logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(builder.Configuration)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
+    builder.Host.UseSerilog(logger);
+
+    builder.Services.AddHttpLogging(options =>
+    {
+        options.LoggingFields = HttpLoggingFields.RequestPropertiesAndHeaders |
+                                HttpLoggingFields.RequestBody |
+                                HttpLoggingFields.ResponsePropertiesAndHeaders |
+                                HttpLoggingFields.ResponseBody;
+    });
+
+    // Add services to the container.
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+    builder.Services.AddWebUIServices();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+
+        // Initialise and seed database
+        // using (var scope = app.Services.CreateScope())
+        // {
+        //     var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+        //     await initialiser.InitialiseAsync();
+        //     await initialiser.SeedAsync();
+        // }
+    }
+    else
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        // app.UseHsts();
+    }
+    app.UseSerilogRequestLogging();
+
+    app.UseHealthChecks("/health");
+    // app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseSwaggerUi3(settings =>
+    {
+        settings.Path = "/api";
+        settings.DocumentPath = "/api/specification.json";
+    });
+
+    app.UseRouting();
+
+    // app.UseAuthentication();
+    // app.UseIdentityServer();
+    // app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    app.MapRazorPages();
+
+    app.MapFallbackToFile("index.html");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
 }
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddWebUIServices();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-
-    // Initialise and seed database
-    // using (var scope = app.Services.CreateScope())
-    // {
-    //     var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
-    //     await initialiser.InitialiseAsync();
-    //     await initialiser.SeedAsync();
-    // }
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    // app.UseHsts();
-}
-
-app.UseHealthChecks("/health");
-// app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseSwaggerUi3(settings =>
-{
-    settings.Path = "/api";
-    settings.DocumentPath = "/api/specification.json";
-});
-
-app.UseRouting();
-
-// app.UseAuthentication();
-// app.UseIdentityServer();
-// app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-app.MapFallbackToFile("index.html");
-
-app.Run();
+// Make the implicit Program class public so test projects can access it
+public partial class Program { }
