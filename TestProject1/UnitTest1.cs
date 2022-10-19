008cf598c41265e65cac04f65d62f8c9e37b5985
Jason Taylor <jasongt@outlook.com>
2022-10-19 12:12:07 +1000

    📦 Update to .NET 7

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestProject1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
--- src/WebUI/Controllers/OidcConfigurationController.cs
+++ src/WebUI/Controllers/OidcConfigurationController.cs
@@ -6,18 +6,20 @@ namespace CleanArchitecture.WebUI.Controllers;
 [ApiExplorerSettings(IgnoreApi = true)]
 public class OidcConfigurationController : Controller
 {
-    private readonly ILogger<OidcConfigurationController> logger;
+    private readonly ILogger<OidcConfigurationController> _logger;
 
-    public OidcConfigurationController(IClientRequestParametersProvider clientRequestParametersProvider, ILogger<OidcConfigurationController> _logger)
+    public OidcConfigurationController(
+        IClientRequestParametersProvider clientRequestParametersProvider,
+        ILogger<OidcConfigurationController> logger)
     {
         ClientRequestParametersProvider = clientRequestParametersProvider;
-        logger = _logger;
+        _logger = logger;
     }
 
     public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
 
     [HttpGet("_configuration/{clientId}")]
-    public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+    public IActionResult GetClientRequestParameters([FromRoute]string clientId)
     {
         var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
         return Ok(parameters);
