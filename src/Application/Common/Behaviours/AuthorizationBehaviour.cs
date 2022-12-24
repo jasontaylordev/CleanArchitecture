a40ac066a41505d4c0c9df1ee386f3347fb54b24
Hannan <hannansultan@outlook.com>
2022-12-24 22:02:50 +0500

    Updated MediatR to version 11 and fixed breaking changes

--- src/Application/Common/Behaviours/AuthorizationBehaviour.cs
+++ src/Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -6,7 +6,7 @@ using MediatR;
 
 namespace CleanArchitecture.Application.Common.Behaviours;
 
-public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private readonly ICurrentUserService _currentUserService;
     private readonly IIdentityService _identityService;
@@ -19,7 +19,7 @@ public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRe
         _identityService = identityService;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
 
