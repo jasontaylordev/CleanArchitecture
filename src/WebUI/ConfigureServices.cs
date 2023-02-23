bbb5a7c52c53cf5f77534d6b1a77df170fe833cb
Jason Taylor <jasongt@outlook.com>
2023-02-23 20:05:04 +1000

    🔀 Merge with main

--- src/WebUI/ConfigureServices.cs
+++ src/WebUI/ConfigureServices.cs
@@ -1,11 +1,16 @@
-﻿using CleanArchitecture.Application.Common.Interfaces;
+﻿using System;
+using System.Reflection;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.TodoItems.Commands.CreateTodoItem;
 using CleanArchitecture.Infrastructure.Persistence;
 using CleanArchitecture.WebUI.Filters;
 using CleanArchitecture.WebUI.Services;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using NSwag;
 using NSwag.Generation.Processors.Security;
+using ZymLabs.NSwag.FluentValidation;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -28,12 +33,25 @@ public static class ConfigureServices
 
         services.AddRazorPages();
 
+        services.AddScoped<FluentValidationSchemaProcessor>(provider =>
+        {
+            var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
+            var loggerFactory = provider.GetService<ILoggerFactory>();
+
+            return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
+        });
+
         // Customise default API behaviour
         services.Configure<ApiBehaviorOptions>(options =>
             options.SuppressModelStateInvalidFilter = true);
 
-        services.AddOpenApiDocument(configure =>
+        services.AddOpenApiDocument((configure, serviceProvider) =>
         {
+            var fluentValidationSchemaProcessor = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
+
+            // Add the fluent validations schema processor
+            configure.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+
             configure.Title = "CleanArchitecture API";
             configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
             {
