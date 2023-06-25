c2795a67afe6d1b36e95a7517be8370934b663d8
Jason Taylor <jasongt@outlook.com>
2023-06-26 08:11:33 +1000

    💡 Describe the reasoning behind BaseEntity.Id key type.

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -4,6 +4,8 @@ namespace CleanArchitecture.Domain.Common;
 
 public abstract class BaseEntity
 {
+    // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
+    // Using non-generic integer types for simplicity
     public int Id { get; set; }
 
     private readonly List<BaseEvent> _domainEvents = new();
