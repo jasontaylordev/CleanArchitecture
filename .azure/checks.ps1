afbe15e0cd23fe9e62e3f67177296266ee159fcf
Jason Taylor <jasongt@outlook.com>
2023-07-26 20:07:18 +1000

    🐛 Improve checks for az and gh.

--- .azure/checks.ps1
+++ .azure/checks.ps1
@@ -1,27 +1,27 @@
 # Check if Azure CLI is installed
 $azPath = (Get-Command az -ErrorAction SilentlyContinue).Source
 if (-not $azPath) {
-    Write-Host "Azure CLI (az) is not installed. Please install it and try again."
-    return
+    Write-Host "Azure CLI (az) is not installed. Please install it and try again." -ForegroundColor Red
+    exit 1
+}
+
+# Check if Azure CLI is authenticated
+az account show --output none
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Azure CLI (az) is not authenticated. Please authenticate with Azure CLI and try again." -ForegroundColor Red
+    exit 1
 }
 
 # Check if GitHub CLI is installed
 $ghPath = (Get-Command gh -ErrorAction SilentlyContinue).Source
 if (-not $ghPath) {
-    Write-Host "GitHub CLI (gh) is not installed. Please install it and try again."
-    return
-}
-
-# Check if Azure CLI is authenticated
-$azLoggedIn = (az account show -o json) -ne $null
-if (-not $azLoggedIn) {
-    Write-Host "Azure CLI (az) is not authenticated. Please authenticate with Azure CLI and try again."
-    return
+    Write-Host "GitHub CLI (gh) is not installed. Please install it and try again." -ForegroundColor Red
+    exit 1
 }
 
 # Check if GitHub CLI is authenticated
-$ghAuthStatus = (gh auth status)
-if (-not ($ghAuthStatus -like "*Logged in to github.com*")) {
-    Write-Host "GitHub CLI (gh) is not authenticated. Please authenticate with GitHub CLI and try again."
-    return
+gh auth status | Out-Null
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "GitHub CLI (gh) is not authenticated. Please authenticate with GitHub CLI and try again." -ForegroundColor Red
+    exit 1
 }
