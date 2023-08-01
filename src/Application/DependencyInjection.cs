5b4f07c5beeb1629dfdd0d4733e920da1047eeb4
Jason Taylor <jasongt@outlook.com>
2023-08-01 20:40:59 +1000

    🚚 Rename to clarfiy intent.

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -3,7 +3,7 @@ using CleanArchitecture.Application.Common.Behaviours;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-public static class ConfigureServices
+public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
