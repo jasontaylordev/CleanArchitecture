fa6cbd5bb7dcb3cedf01774f3cf31b67d8bc5a4c
Christian Wengel <christian.wengel@gmail.com>
2023-08-29 10:51:39 +0200

    🔥 Removed UseMigrationsEndPoint feature.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -14,7 +14,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
     await app.InitialiseDatabaseAsync();
 }
 else
