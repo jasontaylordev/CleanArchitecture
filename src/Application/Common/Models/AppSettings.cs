8e84a98bcff62716129e8a3ed302fbecd6e37f4d
Alex <116060421+n0one42@users.noreply.github.com>
2024-03-05 16:05:05 +0100

    Implementation of strongly typed appsettings

---
+++ src/Application/Common/Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Models;
+
+public class AppSettingsSection
+{
+    public bool PopulateWithDemoData { get; set; } = false;
+}
