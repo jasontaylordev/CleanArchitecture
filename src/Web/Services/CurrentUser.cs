6bbbd2779b456fe0599c7c63dc91f9f2ce54d9c0
antoniolanzolla <antonio.lanzolla@gmail.com>
2024-04-20 12:21:38 +0200

    Implement proposal Improve authorization performance #1046
    Moved Roles authorization from Identity Service DB verification to verify Roles user Claim.

--- src/Web/Services/CurrentUser.cs
+++ src/Web/Services/CurrentUser.cs
@@ -14,4 +14,6 @@ public class CurrentUser : IUser
     }
 
     public string? Id => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    public List<string>? Roles => _httpContextAccessor.HttpContext?.User?.FindAll(ClaimTypes.Role).Select(x => x.Value).ToList();
+
 }
