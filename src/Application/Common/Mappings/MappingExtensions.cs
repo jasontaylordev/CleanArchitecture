f4b22046ddd964149ca20fca62391a6452bb73c1
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-01-17 18:51:40 +0600

    chore: pass cancellation token in paginatedlistasync

--- src/Application/Common/Mappings/MappingExtensions.cs
+++ src/Application/Common/Mappings/MappingExtensions.cs
@@ -4,7 +4,7 @@ namespace CleanArchitecture.Application.Common.Mappings;
 
 public static class MappingExtensions
 {
-    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize, CancellationToken cancellationToken = default) where TDestination : class
         => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
 
     public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
