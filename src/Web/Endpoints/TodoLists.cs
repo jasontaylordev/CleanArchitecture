2f0a7f8da47b64b473074608dec13ca35e8a3f61
Jason Taylor <hello@jasontaylor.dev>
2025-07-17 05:56:50 +1000

    Change to ASP.NET Core Open API + Scalar

--- src/Web/Endpoints/TodoLists.cs
+++ src/Web/Endpoints/TodoLists.cs
@@ -18,6 +18,9 @@ public class TodoLists : EndpointGroupBase
             .MapDelete(DeleteTodoList, "{id}");
     }
 
+    [EndpointName(nameof(GetTodoLists))]
+    [EndpointSummary("Get all Todo Lists")]
+    [EndpointDescription("Retrieves all todo lists along with their items.")]
     public async Task<Ok<TodosVm>> GetTodoLists(ISender sender)
     {
         var vm = await sender.Send(new GetTodosQuery());
@@ -25,6 +28,9 @@ public class TodoLists : EndpointGroupBase
         return TypedResults.Ok(vm);
     }
 
+    [EndpointName(nameof(CreateTodoList))]
+    [EndpointSummary("Create a new Todo List")]
+    [EndpointDescription("Creates a new todo list using the provided details and returns the ID of the created list.")]
     public async Task<Created<int>> CreateTodoList(ISender sender, CreateTodoListCommand command)
     {
         var id = await sender.Send(command);
@@ -32,15 +38,22 @@ public class TodoLists : EndpointGroupBase
         return TypedResults.Created($"/{nameof(TodoLists)}/{id}", id);
     }
 
+    [EndpointName(nameof(UpdateTodoList))]
+    [EndpointSummary("Update a Todo List")]
+    [EndpointDescription("Updates the specified todo list. The ID in the URL must match the ID in the payload.")]
     public async Task<Results<NoContent, BadRequest>> UpdateTodoList(ISender sender, int id, UpdateTodoListCommand command)
     {
-        if (id != command.Id) return TypedResults.BadRequest();
-        
+        if (id != command.Id)
+            return TypedResults.BadRequest();
+
         await sender.Send(command);
 
         return TypedResults.NoContent();
     }
 
+    [EndpointName(nameof(DeleteTodoList))]
+    [EndpointSummary("Delete a Todo List")]
+    [EndpointDescription("Deletes the todo list with the specified ID.")]
     public async Task<NoContent> DeleteTodoList(ISender sender, int id)
     {
         await sender.Send(new DeleteTodoListCommand(id));
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,5 @@
 using CleanArchitecture.Infrastructure.Data;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,11 +31,7 @@ app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseSwaggerUi(settings =>
-{
-    settings.Path = "/api";
-    settings.DocumentPath = "/api/specification.json";
-});
+app.MapScalarApiReference();
 
 #if (!UseApiOnly)
 app.MapRazorPages();
