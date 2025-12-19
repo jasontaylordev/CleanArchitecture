dde5f895233c185a0ec442198a6ab996d5bc92d4
saman azadi <samanazadi1996@gmail.com>
2025-12-19 15:04:50 +0330

    Refactor endpoint registration to use DI

--- src/Web/Infrastructure/WebApplicationExtensions.cs
+++ src/Web/Infrastructure/WebApplicationExtensions.cs
@@ -4,6 +4,34 @@ namespace CleanArchitecture.Web.Infrastructure;
 
 public static class WebApplicationExtensions
 {
+    public static IServiceCollection AddEndpoints(this IServiceCollection services)
+    {
+        var endpointGroupType = typeof(EndpointGroupBase);
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var endpointGroupTypes = assembly.GetExportedTypes()
+            .Where(t => t.IsSubclassOf(endpointGroupType) && !t.IsAbstract);
+
+        foreach (var type in endpointGroupTypes)
+        {
+            services.AddTransient(endpointGroupType, type);
+        }
+
+        return services;
+    }
+
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        var endpointGroups = app.Services.GetServices<EndpointGroupBase>();
+
+        foreach (var instance in endpointGroups)
+        {
+            instance.Map(app.MapGroup(instance));
+        }
+
+        return app;
+    }
+
     private static RouteGroupBuilder MapGroup(this WebApplication app, EndpointGroupBase group)
     {
         var groupName = group.GroupName ?? group.GetType().Name;
@@ -12,25 +40,5 @@ public static class WebApplicationExtensions
             .MapGroup($"/api/{groupName}")
             .WithGroupName(groupName)
             .WithTags(groupName);
-    }
-
-    public static WebApplication MapEndpoints(this WebApplication app)
-    {
-        var endpointGroupType = typeof(EndpointGroupBase);
-
-        var assembly = Assembly.GetExecutingAssembly();
-
-        var endpointGroupTypes = assembly.GetExportedTypes()
-            .Where(t => t.IsSubclassOf(endpointGroupType));
-
-        foreach (var type in endpointGroupTypes)
-        {
-            if (Activator.CreateInstance(type) is EndpointGroupBase instance)
-            {
-                instance.Map(app.MapGroup(instance));
-            }
-        }
-
-        return app;
     }
 }
