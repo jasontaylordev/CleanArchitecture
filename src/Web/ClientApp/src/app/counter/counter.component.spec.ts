4692562faf68b9236ae05a2139ef23107a69fb00
jasontaylordev <hello@jasontaylor.dev>
2026-02-23 21:15:05 +1000

    Fix Angular template issues: deprecated async and missing tsconfig.server.json

--- src/Web/ClientApp/src/app/counter/counter.component.spec.ts
+++ src/Web/ClientApp/src/app/counter/counter.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
   let fixture: ComponentFixture<CounterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CounterComponent ]
     })
@@ -17,12 +17,12 @@ describe('CounterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should display a title', async(() => {
+  it('should display a title', waitForAsync(() => {
     const titleText = fixture.nativeElement.querySelector('h1').textContent;
     expect(titleText).toEqual('Counter');
   }));
 
-  it('should start with count 0, then increments by 1 when clicked', async(() => {
+  it('should start with count 0, then increments by 1 when clicked', waitForAsync(() => {
     const countElement = fixture.nativeElement.querySelector('strong');
     expect(countElement.textContent).toEqual('0');
 
